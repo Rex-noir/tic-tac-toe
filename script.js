ef7ae3cf6b10836e825b21e58a3851a4f086fa62
AceReX <ace.rexnoir@gmail.com>
2024-04-17 00:02:14 +0530

    Finished the backend side v1

--- script.js
+++ script.js
@@ -43,12 +43,14 @@ const gameManager = () => {
         return column[0].value;
       }
     }
+
     //for diagonally
-    const diagonal1 = [board[0][1], board[1][1], board[2][2]];
+    const diagonal1 = [board[0][0], board[1][1], board[2][2]];
     const diagonal2 = [board[0][2], board[1][1], board[2][0]];
-    if (checkLine(diagonal1) || checkLine(diagonal2)) {
-      return diagonal1[0].value;
-    }
+
+    if (checkLine(diagonal1)) return diagonal1[0].value;
+    else if (checkLine(diagonal2)) return diagonal2[0].value;
+
     //check for tie
     if (board.every((row) => row.every((cell) => cell.value !== null))) {
       return "Tie";
@@ -87,36 +89,111 @@ const documentMock = (() => ({
 
 //UI Manager
 const UIManager = ((document) => {
+  let users;
+  //callback for the form
+  const startTheGame = function (e) {
+    e.preventDefault();
+    let form = new FormData(e.target);
+    let player1 = form.get("player1");
+    let player2 = form.get("player2");
+    let btn = document.querySelector(".name-submit-button");
+    if (player1 == "" || player2 == "") {
+      showMessage("Please don't leave the name fields blank!");
+    } else {
+      users = game.createPlayers(player1, player2);
+      startUI();
+      btn.disabled = true;
+    }
+  };
+  //create the dialog to show message
+  const showMessage = (message) => {
+    let dialog = createElement("dialog", "dialog");
+    let title = createElement("h3");
+    let msg = createElement("span");
+    let close = createElement("button", "close-dialog");
+
+    close.textContent = "Close";
+    title.textContent = "Notice!";
+    msg.textContent = message;
+
+    close.addEventListener("click", (e) => {
+      dialog.close();
+      if (message !== "Please don't leave the name fields blank!") {
+        window.location.reload();
+      }
+    });
+
+    dialog.appendChild(title);
+    dialog.appendChild(msg);
+    dialog.append(close);
+    container.appendChild(dialog);
+    dialog.showModal();
+  };
+  //require for inputs first
+  const setUp = () => {
+    let input_container = createElement("div", "input-container");
+    let form = createElement("form", "users-form");
+
+    let player1Label = createElement("label", "input-label");
+    let player2Label = createElement("label", "input-label");
+    player1Label.textContent = "Player1";
+    player2Label.textContent = "Player2";
+
+    player1Label.setAttribute("for", "player1-name");
+    player2Label.setAttribute("for", "player2-name");
+
+    let player1 = createElement("input", "player-name", "player1-name");
+    let player2 = createElement("input", "player-name", "player2-name");
+    player1.setAttribute("type", "text");
+    player2.setAttribute("type", "text");
+
+    player1.setAttribute("value", "Player1");
+    player2.setAttribute("value", "Player2");
+    player1.setAttribute("name", "player1");
+    player2.setAttribute("name", "player2");
+
+    let submitBtn = createElement("input", "name-submit-button");
+    submitBtn.setAttribute("type", "submit");
+    submitBtn.value = "Start";
+
+    form.addEventListener("submit", startTheGame);
+
+    form.appendChild(player1Label);
+    form.appendChild(player1);
+    form.appendChild(player2Label);
+    form.appendChild(player2);
+    form.appendChild(submitBtn);
+    input_container.appendChild(form);
+    container.appendChild(input_container);
+  };
   //start building the UI
   const startUI = () => {
-    let layout = createLayout("wrapper");
-    container = layout.container;
+    console.log(users);
     const board = createBoardUI();
     container.appendChild(board);
-    document.body.appendChild(layout.wrapper);
   };
-
-  //Create The Game Here
-  let active = "X";
-  let board = boardManger();
-  board.createBoard();
-  let boardJS = board.board;
-
-  let game = gameManager();
-  let users = game.createPlayers("Jujutsu", "Lily");
-
   //click callback
+
   const cellsClicked = (e) => {
     let element = e.target;
     let coordinate = element.value.match(/\d+/g);
-
     //update per click
     element.textContent = active;
     board.updateBoard(active, coordinate, boardJS);
     element.disabled = true;
     //Change turns
+
+    let winner = game.checkWinner("X", "0", boardJS);
+    if (winner) {
+      if (winner == "X" || winner == "O") {
+        winner = `Congratulations! \n ${users.symbolToName[winner]} wins!!`;
+      }
+      showMessage(`${winner}`);
+      console.log("Helll");
+    }
     changeActive();
   };
+  //changing turns fn
   const changeActive = () => {
     active =
       active === users.nameToSymbol.player1
@@ -168,7 +245,20 @@ const UIManager = ((document) => {
     if (id) element.id = id;
     return element;
   };
+  //Create The Game Here
+  let active = "X";
+  let board = boardManger();
+  board.createBoard();
+  let boardJS = board.board;
+
+  let game = gameManager();
+
+  let layout = createLayout("wrapper");
+  let container = layout.container;
+  document.body.appendChild(layout.wrapper);
+
   return {
+    setUp,
     startUI,
     createLayout,
     createElement,
@@ -176,4 +266,4 @@ const UIManager = ((document) => {
 })(document || documentMock);
 
 let ui = UIManager;
-ui.startUI();
+ui.setUp();
